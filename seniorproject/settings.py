2a5cf241f6787cb79909efec2122727ccc356e50
mathatpongchakoa <mathat.po.2546@gmail.com>
2025-08-27 15:55:31 +0700

    newport

--- seniorproject/settings.py
+++ seniorproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-z7+xcwcx#x=2oz(5n1gjo5mr7^ussz!lk-lhjwdmsiod#g#wgv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
